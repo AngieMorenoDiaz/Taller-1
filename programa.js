981ad6f93ed1d55f9db2fb3a37959d71d277fd52
Angie Moreno <angievalemd@gmail.com>
2023-03-06 14:26:53 -0500

    Version inicial

---
+++ programa.js
@@ -0,0 +1,30 @@
+//TALLER 
+//1. Definir dos variables, asignarles un valor y utilizar decisiones imprimir en la 
+// consola la que tenga el menor valor
+
+let a= 15;
+let b= 24;
+
+if (a<b){
+    console.log ("La variable con menor valor es a:"+ a);
+}
+    else {("La variable con menor valor es b:"+ b);
+} 
+
+
+// 2. Definir tres variables, asignarles valores y utilizar decisiones para imprimir
+// en la que tenga el mayor valor
+
+let j = 21;
+let k = 25;
+let l= 1;
+
+if (j>k && j>l) {
+    console.log ("La variable con menor valor es j:" + j);
+} else if (k>j && k>l){
+    console.log("La variable con menor valor es k:" +  k);
+} else {
+    console.log ("La variBLE CON MAYOR VALOR ES l:" + l);
+}
+
+
